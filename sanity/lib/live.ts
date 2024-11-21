3516ac282c008c0a5c84fb9819246fcac6499f52
EHTISHAM-Afzal <khaj3172@gmail.com>
2024-11-21 11:51:26 +0500

    Changes to be committed: 	modified:   sanity/lib/live.ts

--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -14,7 +14,7 @@ export const { sanityFetch, SanityLive } = defineLive({
   client: client.withConfig({
     // Live content is currently only available on the experimental API
     // https://www.sanity.io/docs/api-versioning
-    apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-11-21'
+    apiVersion: 'vX'
   }),
   serverToken: token,
   browserToken: token,
